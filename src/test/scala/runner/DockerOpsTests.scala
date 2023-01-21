0c431f87881970381c9f7db4be0258518931a591
azu0nyan <nikgavgav@gmail.com>
2023-01-22 02:21:56 +0300

    tests for failed compilation

--- src/test/scala/runner/DockerOpsTests.scala
+++ src/test/scala/runner/DockerOpsTests.scala
@@ -16,6 +16,7 @@ object DockerOpsTests extends ZIOSpecDefault {
       val tarStream = CompressOps.asTarStream(javaFileText, "Main.java")
 
       val testContainerName = "cont:0.1"
+      val failedCommand = Seq("javac", "Main.jav")
       val compileCommand = Seq("javac", "Main.java")
       val runCommand = Seq("java", "Main")
       val input = new ByteArrayInputStream("2".getBytes("UTF-8"))
@@ -24,14 +25,16 @@ object DockerOpsTests extends ZIOSpecDefault {
         _ <- DockerOps.copyArchiveToContainer(CopyArchiveToContainerParams("/", tarStream))
         res <- DockerOps.executeCommandInContainer(ExecuteCommandParams(compileCommand, None))
         res2 <- DockerOps.executeCommandInContainer(ExecuteCommandParams(runCommand, Some(input)))
-      } yield (res, res2)
+        res3 <- DockerOps.executeCommandInContainer(ExecuteCommandParams(failedCommand, None))
+      } yield (res, res2, res3)
 
       val res = for (r <- DockerOps.doInContainer(testContainerName)(toRunInContainer).provideLayer(DockerOps.clientLayer)) yield r
 
       assertZIO(res)(
-        hasField("firstResult", (x: (ExecuteCommandResult, ExecuteCommandResult)) => x._1, equalTo(ExecuteCommandResult(Some(0), "", ""))) &&
-          hasField("secondResult", (x: (ExecuteCommandResult, ExecuteCommandResult)) => x._2, equalTo(ExecuteCommandResult(Some(0), "RESPONSE2\n", "")))
-
+        hasField("firstResult", (x: (ExecuteCommandResult, ExecuteCommandResult, ExecuteCommandResult)) => x._1, equalTo(ExecuteCommandResult(Some(0), "", ""))) &&
+          hasField("secondResult", (x: (ExecuteCommandResult, ExecuteCommandResult, ExecuteCommandResult)) => x._2, equalTo(ExecuteCommandResult(Some(0), "RESPONSE2\n", ""))) &&
+          hasField("thirdCode", (x: (ExecuteCommandResult, ExecuteCommandResult, ExecuteCommandResult)) => x._3.exitCode, equalTo(Some(1))) &&
+          hasField("thirdErrorText", (x: (ExecuteCommandResult, ExecuteCommandResult, ExecuteCommandResult)) => x._3.stdErr.toSeq, isNonEmpty)
       )
 
     }
